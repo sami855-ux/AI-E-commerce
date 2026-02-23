0a5c120ac46e150204f3f09a9a706851edf8cc54
sami855-ux <samitale56@outlook.com>
2026-02-23 21:58:51 +0300

    feat(app): fetch and load all categories on homepage
    - Import sanityFetch function and ALL_CATEGORIES_QUERY
    - Modify Home component to be async for data fetching
    - Fetch categories data from sanity backend before rendering
    - Prepare homepage for dynamic category content display

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { sanityFetch } from "@/sanity/lib/live"
+import { ALL_CATEGORIES_QUERY } from "@/sanity/queries/categories"
+
+export default async function Home() {
+  const {data: catagories} = await sanityFetch({query: ALL_CATEGORIES_QUERY})
+
   return (
     <div className="">
       {/*Featured Prodcuts Carousole */}
